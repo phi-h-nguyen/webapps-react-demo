3fa31c50f6a1ef23713bb782df7c1a9c790d9c93
Phi Henry Nguyen <phi.ng1114@gmail.com>
2023-03-25 11:02:27 -0400

    Final game

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import "../pages/Game.css"
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
